5089eb2b97330141b5eb823e5f4ceafd0878f642
Cylon <cylon2015@gmail.com>
2023-05-01 08:53:15 -0300

    Login e CORS

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,7 +3,9 @@ using System;
 using CylonApi.Repositories;
 using CylonApi.Models;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace CylonApi.Controllers
 {
@@ -21,12 +23,51 @@ namespace CylonApi.Controllers
         public IActionResult Listar(){
             return Ok(_usuarioRepository.Listar());
         }
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        // _usuarioRepository.Cadastrar(usuario);
+        // return StatusCode(201);
+        // }
+
+        public IActionResult Post(Usuario usuario)
         {
-        _usuarioRepository.Cadastrar(usuario);
-        return StatusCode(201);
+        Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email, 
+        usuario.Senha);
+        if (usuarioBuscado == null)
+        {
+        return NotFound("E-mail ou senha inválidos!");
         }
+        // Se o usuário for encontrado, segue a criação do token.
+        // Define os dados que serão fornecidos no token - Payload.
+        var claims = new[]
+        {
+        // Armazena na claim o e-mail usuário autenticado.
+        new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+        // Armazena na claim o id do usuário autenticado.
+        new Claim(JwtRegisteredClaimNames.Jti, 
+        usuarioBuscado.Id.ToString()),
+        };
+        // Define a chave de acesso ao token.
+        var key = new
+        SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao"));
+        // Define as credenciais do token.
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        // Gera o token.
+        var token = new JwtSecurityToken(
+        issuer: "exoapi.webapi", // Emissor do token.
+        audience: "exoapi.webapi", // Destinatário do token.
+        claims: claims, // Dados definidos acima.
+        expires: DateTime.Now.AddMinutes(30), // Tempo de expiração.
+        signingCredentials: creds // Credenciais do token.
+        );
+        // Retorna ok com o token.
+        return Ok(
+        new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+        );
+        }
+
+
         [HttpGet("{id}")] // Faz a busca pelo ID.
         public IActionResult BuscarPorId(int id)
         {
@@ -37,12 +78,15 @@ namespace CylonApi.Controllers
         }
         return Ok(usuario);
         }
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
         _usuarioRepository.Atualizar(id, usuario);
         return StatusCode(204);
         }
+
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
--- Program.cs
+++ Program.cs
@@ -5,12 +5,45 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Autenticação.
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme = "JwtBearer";
+options.DefaultChallengeScheme = "JwtBearer";
+})
+// Parâmetros de validacão do token.
+.AddJwtBearer("JwtBearer", options =>
+{
+options.TokenValidationParameters = new TokenValidationParameters
+{
+// Valida quem está solicitando.
+ValidateIssuer = true,
+// Valida quem está recebendo.
+ValidateAudience = true,
+// Define se o tempo de expiração será validado.
+ValidateLifetime = true,
+// Criptografia e validação da chave de autenticacão.
+IssuerSigningKey = new
+SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao")),
+// Valida o tempo de expiração do token.
+ClockSkew = TimeSpan.FromMinutes(30),
+// Nome do issuer, da origem.
+ValidIssuer = "exoapi.webapi",
+// Nome do audience, para o destino.
+ValidAudience = "exoapi.webapi"
+};
+});
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,6 +63,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+
 
 app.UseAuthorization();
 
